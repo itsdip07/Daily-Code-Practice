7f490efd840c5199b514723b3246043682de065a
Dipayan Maiti <124234691+itsdip07@users.noreply.github.com>
2026-09-14 22:19:01 +0530

    Add Pen class with write method in Oops.java

---
+++ Java/ Day-001/Oops.java
@@ -0,0 +1,21 @@
+class Pen {
+    String color;
+    String type;
+
+    public void write(){
+        System.out.println("Writing Something...");
+    }
+
+}
+
+public class Oops {
+    public static void main(String[] args) {
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        pen1.write();
+
+
+    }
+}
